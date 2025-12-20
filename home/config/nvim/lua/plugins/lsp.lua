530759d96c14e282e45a67e8989ec702df3880ba
Rishab Garg <me@rishab-garg.me>
2025-12-20 18:28:03 +0100

    feat: update lsps

--- home/config/nvim/lua/plugins/lsp.lua
+++ home/config/nvim/lua/plugins/lsp.lua
@@ -65,12 +65,14 @@ return {
 							globalPlugins = {
 								{
 									name = "@astrojs/ts-plugin",
+									-- TODO:
 									location = vim.fn.stdpath("data")
 										.. "/mason/packages/astro-language-server/node_modules/@astrojs/ts-plugin",
 									enableForWorkspaceTypeScriptVersions = true,
 								},
 								{
 									name = "typescript-svelte-plugin",
+									-- TODO:
 									location = vim.fn.stdpath("data")
 										.. "/mason/packages/svelte-language-server/node_modules/typescript-svelte-plugin",
 									enableForWorkspaceTypeScriptVersions = true,
@@ -91,6 +93,10 @@ return {
 				keys = {
 					{ "<Leader>K", "<plug>(vimtex-doc-package)", desc = "Vimtex Docs", silent = true },
 				},
+			})
+
+			vim.lsp.config("tailwind", {
+				filetypes = { "astro" },
 			})
 		end,
 		dependencies = {
