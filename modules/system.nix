760af279d217d6b8a1d6fc4a499ee374c6bc047b
Rishab Garg <me@rishab-garg.me>
2025-12-24 21:25:34 +0100

    fix: remove blocked-names

--- modules/system.nix
+++ modules/system.nix
@@ -99,27 +99,41 @@
   services.dnscrypt-proxy = {
     enable = true;
     settings = {
+      ipv4_servers = true;
       ipv6_servers = true;
+
+      dnscrypt_servers = true;
+      doh_servers = true;
+
       require_dnssec = true;
+      require_nolog = true;
+      require_nofilter = true;
+
       http3 = true;
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-        ];
-        cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+
+      sources = {
+        public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+        relays = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/relays.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/relays.md'"
+          ];
+          cache_file = "relays.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
       };
+
       forwarding_rules = ./forwarding-rules.conf;
+
       monitoring_ui = {
         enabled = true;
-        listen_address = "127.0.0.1:8080";
-        username = "admin";
-        password = "changeme";
-        tls_certificate = "";
-        tls_key = "";
-        enable_query_log = true;
-        privacy_level = 1;
       };
     };
 
