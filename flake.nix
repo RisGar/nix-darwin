b4e687a24e6d3ad9dd87bdb5fe74644f949d9587
Rishab Garg <me@rishab-garg.me>
2026-01-04 19:30:58 +0100

    feat: add homelab config

--- flake.nix
+++ flake.nix
@@ -1,34 +1,141 @@
 {
-  description = "nix-darwin system flake";
+  description = "nix-darwin and nixos system flake";
+
+  outputs =
+    {
+      # homebrew-cask,
+      # homebrew-core,
+      # homebrew-sikarugir,
+      home-manager,
+      mlpreview,
+      nix-colors,
+      nix-darwin,
+      nix-homebrew,
+      nixln-edit,
+      nixos,
+      nixpkgs,
+      nvim-config,
+      self,
+      ...
+    }:
+    let
+      darwinPkgs = import nixpkgs rec {
+        system = "aarch64-darwin";
+        overlays = [
+          mlpreview.overlays.default
+          (final: prev: { nixln-edit = nixln-edit.packages.${system}.default; })
+        ];
+        config = {
+          allowUnfree = true;
+        };
+      };
+      linuxPkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ ];
+        config = { };
+      };
+      secrets = builtins.fromTOML (builtins.readFile ./secrets.toml);
+    in
+    {
+      # Build darwin flake using:
+      # $ sudo -i darwin-rebuild build --flake .#Rishabs-MacBook-Pro
+      darwinConfigurations."Rishabs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        system = darwinPkgs.system;
+        modules = [
+          ./hosts/macbook
+
+          home-manager.darwinModules.home-manager
+          nix-homebrew.darwinModules.nix-homebrew
+
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users."rishab" = ./home/rishab;
+              backupFileExtension = "bak";
+
+              extraSpecialArgs = {
+                inherit nvim-config;
+                inherit nix-colors;
+              };
+            };
+
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = false;
+              user = "rishab";
+              autoMigrate = true;
+              # Declarative tap management
+              # TODO: can I make this work without uninstalling homebrew?
+              # taps = {
+              #   "homebrew/homebrew-core" = homebrew-core;
+              #   "homebrew/homebrew-cask" = homebrew-cask;
+              #   "sikarugir-app/homebrew-sikarugir" = homebrew-sikarugir;
+              # };
+              # mutableTaps = true;
+            };
+          }
+
+        ];
+        specialArgs = {
+          inherit darwinPkgs;
+          inherit secrets;
+          inherit self;
+        };
+      };
+
+      nixosConfigurations."Rishabs-Homelab" = nixos.lib.nixosSystem {
+        system = linuxPkgs.system;
+
+        modules = [
+          ./hosts/homelab
+        ];
+
+        specialArgs = {
+          inherit linuxPkgs;
+          inherit secrets;
+          inherit self;
+        };
+      };
+    };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+    nixos = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     mlpreview = {
       url = "github:RisGar/mlpreview";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-
+    nix-homebrew = {
+      url = "github:zhaofengli/nix-homebrew";
+    };
     nixln-edit = {
       url = "github:nlintn/nixln-edit";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nvim-config = {
-      url = "path:///Users/rishab/Documents/Programming/nvim-config";
+      url = "github:RisGar/nvim-config";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    base16-schemes = {
+      url = "github:RisGar/base16-schemes";
+      flake = false;
+    };
+    nix-colors = {
+      url = "github:misterio77/nix-colors";
+      inputs.base16-schemes.follows = "base16-schemes";
     };
 
     # Homebrew taps
@@ -45,91 +152,4 @@
     #   flake = false;
     # };
   };
-  outputs =
-    {
-      # homebrew-cask,
-      # homebrew-core,
-      # homebrew-sikarugir,
-      home-manager,
-      mlpreview,
-      nix-darwin,
-      nix-homebrew,
-      nixln-edit,
-      nixpkgs,
-      nvim-config,
-      self,
-      ...
-    }:
-    let
-      system = "aarch64-darwin";
-    in
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          mlpreview.overlays.default
-          (final: prev: { nixln-edit = nixln-edit.packages.${system}.default; })
-        ];
-        config = {
-          allowUnfree = true;
-        };
-      };
-      secrets = builtins.fromTOML (builtins.readFile ./secrets/secrets.toml);
-    in
-    {
-      # Build darwin flake using:
-      # $ sudo -i darwin-rebuild build --flake .#Rishabs-MacBook-Pro
-      darwinConfigurations."Rishabs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-        modules = [
-          ./modules/system.nix
-          ./modules/brew.nix
-
-          home-manager.darwinModules.home-manager
-          nix-homebrew.darwinModules.nix-homebrew
-
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users."rishab" = ./home;
-              backupFileExtension = "bak";
-
-              extraSpecialArgs = {
-                inherit pkgs;
-                inherit nvim-config;
-              };
-            };
-
-            nix-homebrew = {
-              # Install Homebrew under the default prefix
-              enable = true;
-
-              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-              enableRosetta = false;
-
-              # User owning the Homebrew prefix
-              user = "rishab";
-
-              # Automatically migrate existing Homebrew installations
-              autoMigrate = true;
-
-              # Declarative tap management
-              # TODO: can I make this work without uninstalling homebrew?
-              # taps = {
-              #   "homebrew/homebrew-core" = homebrew-core;
-              #   "homebrew/homebrew-cask" = homebrew-cask;
-              #   "sikarugir-app/homebrew-sikarugir" = homebrew-sikarugir;
-              # };
-              # mutableTaps = true;
-            };
-          }
-
-        ];
-        specialArgs = {
-          inherit pkgs;
-          inherit secrets;
-          inherit self;
-        };
-      };
-    };
 }
---
+++ hosts/homelab/default.nix
@@ -0,0 +1,7 @@
+{
+  linuxPkgs,
+  ...
+}:
+{
+  nixpkgs.pkgs = linuxPkgs;
+}
