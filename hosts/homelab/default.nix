e2a62e7bf37cde272dd0e3b7fcad503dcaeae50f
Rishab Garg <me@rishab-garg.me>
2026-01-29 21:04:55 +0100

    feat: modularise homelab config

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -1,5 +1,4 @@
 {
-  linuxPkgs,
   modulesPath,
   pkgs,
   config,
@@ -11,6 +10,11 @@
     (modulesPath + "/profiles/qemu-guest.nix")
     ./disk-config.nix
     ./hardware-configuration.nix
+    ./metrics.nix
+    ./nfs.nix
+    ./samba.nix
+    ./transmission.nix
+    ./yopass.nix
   ];
 
   # nixos-anywhere --flake .#homelab --generate-hardware-config nixos-generate-config ./hosts/homelab/hardware-configuration.nix <hostname>
@@ -30,9 +34,7 @@
   environment.systemPackages = with pkgs; [
     vim
     git
-    podman
     podman-compose
-    podman-tui
   ];
 
   # security.sudo.wheelNeedsPassword = false; # Don't ask for passwords
@@ -61,50 +63,31 @@
       "samba"
       "users"
     ];
-    linger = true;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO9DpYnih3E0xt3A7ra5h+ecrpXf0+/aZjcmAI/0YkWZ"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPktLCANAvs2+iMzZqzLx5mSi5PrYZmWoPTQK112Eyst"
     ];
   };
 
-  nixpkgs.pkgs = linuxPkgs;
-
   system.stateVersion = "25.11";
 
-  nix.settings.trusted-users = [
-    "root"
-    "@wheel"
-  ];
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-    "pipe-operators"
-  ];
-  virtualisation = {
-    containers.enable = true;
+  nix.settings = {
+    trusted-users = [
+      "root"
+      "@wheel"
+    ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+      "pipe-operators"
+    ];
+  };
 
-    oci-containers.containers = {
-      transmission-openvpn = {
-        volumes = [
-          "/mnt/external/Transmission:/data"
-          "/home/rishab/docker/torrent/config:/config"
-        ];
-        environmentFiles = [ config.age.secrets.transmission-openvpn.path ];
-        environment = {
-          OPENVPN_PROVIDER = "NORDVPN";
-          NORDVPN_PROTOCOL = "tcp";
-          NORDVPN_CATEGORY = "legacy_p2p";
-          NORDVPN_COUNTRY = "DE";
-          TRANSMISSION_DOWNLOAD_DIR = "/data/completed";
-          TZ = "Europe/Berlin";
-          LOCAL_NETWORK = "192.168.178.0/24";
-        };
-        log-driver = "json-file";
-        ports = [ "9091:9091" ];
-        image = "haugene/transmission-openvpn";
-        privileged = true;
-      };
-    };
+  virtualisation.containers.enable = true;
+  virtualisation.oci-containers.backend = "podman";
+  virtualisation.podman = {
+    enable = true;
+    dockerSocket.enable = true;
+    defaultNetwork.settings.dns_enabled = true;
   };
 
   fileSystems = {
@@ -118,28 +101,19 @@
     };
   };
 
-  services.nfs.server = {
-    enable = true;
-    exports = ''
-      /export          192.168.178.0/24(rw,fsid=0,no_subtree_check,crossmnt,insecure)
-      /export/external 192.168.178.0/24(rw,nohide,insecure,no_subtree_check)
-    '';
-  };
-
   networking = {
     hostName = "Rishabs-Homelab";
 
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [
-        2049
-      ];
-    };
+    firewall.enable = true;
+
+    usePredictableInterfaceNames = true;
 
     defaultGateway = "192.168.178.1";
-    nameservers = [ "8.8.8.8" ];
+    nameservers = [
+      "1.1.1.1"
+      "8.8.8.8"
+    ];
 
-    useDHCP = false;
     interfaces.ens18.ipv4.addresses = [
       {
         address = "192.168.178.42";
@@ -153,29 +127,20 @@
     execWheelOnly = true;
   };
 
-  services.samba = {
+  services.newt = {
     enable = true;
-    # package = pkgs.samba4Full;
-    usershares.enable = true;
-    openFirewall = true;
     settings = {
-      "tm_share" = {
-        "path" = "/mnt/external/TimeMachine";
-        "valid users" = "rishab";
-        "public" = "no";
-        "writeable" = "yes";
-        "fruit:aapl" = "yes";
-        "fruit:time machine" = "yes";
-        "fruit:time machine max size" = "1T";
-        "vfs objects" = "catia fruit streams_xattr";
-      };
+      endpoint = "https://pangolin.homelab.rishab-garg.me";
+      docker-socket = "unix:///var/run/docker.sock";
     };
+    environmentFile = config.age.secrets.newt.path;
   };
 
-  system.activationScripts = {
-    init_smbpasswd.text = ''
-      /run/current-system/sw/bin/printf "$(/run/current-system/sw/bin/cat ${config.age.secrets.homelab-rishab.path})\n$(/run/current-system/sw/bin/cat ${config.age.secrets.homelab-rishab.path})\n" | /run/current-system/sw/bin/smbpasswd -sa rishab
-    '';
+  services.stirling-pdf = {
+    enable = true;
+    environment = {
+      SECURITY_ENABLELOGIN = "true";
+    };
   };
 
 }
