08ba182fabffc7374f2f93529dc9a5a8a0fab96c
Rishab Garg <mail@rishab-garg.de>
2026-06-23 12:05:51 +0200

    chore(secrets): update

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,6 +1,7 @@
 lib:
 
-lib.listToAttrs
-<| lib.map (name: lib.nameValuePair (lib.removeSuffix ".age" name) { file = ./${name}; })
-<| lib.attrNames
-<| (import ./secrets.nix)
+lib.listToAttrs (
+  lib.map (name: lib.nameValuePair (lib.removeSuffix ".age" name) { file = ./${name}; }) (
+    lib.attrNames (import ./secrets.nix)
+  )
+)
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,32 +1,27 @@
 let
+  # User / Admin Keys (can decrypt everything to edit)
   rishabs-macbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJWmextJLZ2oObSyUPW0pqUq9nnSWuZkieDB+CaY9hLy";
-  rishabs-homelab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+74XpLMdeUesLv6mGZIdqItpITQexLj9IVdeuzAnV5";
 
-  keys = [
+  # Host Keys (can decrypt only what they need)
+  rishabs-homelab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+74XpLMdeUesLv6mGZIdqItpITQexLj9IVdeuzAnV5";
+  # vps = "ssh-ed25519 ..."; # TODO: Add your VPS public key here
+
+  # Groups
+  homelab_keys = [
     rishabs-macbook
     rishabs-homelab
   ];
-
-  secrets = [
-    "homelab-rishab"
-    "homelab-root"
-    "transmission-openvpn"
-    "pangolin"
-    "newt"
-    "pocket-id"
-    "context7"
-    "tavily"
-    "anilist-mal-sync"
-    "open-webui"
-    "homarr"
-    "cloudflared"
-    "openrouter"
-    "openclaw"
-  ];
 in
-builtins.listToAttrs
-<| map (secret: {
-  name = secret + ".age";
-  value.publicKeys = keys;
-})
-<| secrets
+{
+  # Homelab only
+  "anilist-mal-sync.age".publicKeys = homelab_keys;
+  "context7.age".publicKeys = homelab_keys;
+  "nordvpn.age".publicKeys = homelab_keys;
+  "openrouter.age".publicKeys = homelab_keys;
+  "pocket-id.age".publicKeys = homelab_keys;
+  "tandoor.age".publicKeys = homelab_keys;
+  "tavily.age".publicKeys = homelab_keys;
+  "user-rishab.age".publicKeys = homelab_keys;
+  "user-root.age".publicKeys = homelab_keys;
+  "wg-private.age".publicKeys = homelab_keys;
+}
